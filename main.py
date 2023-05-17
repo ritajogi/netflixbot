57770343c71af59d976ce35ca554aaa9be3fedd5
ritajogi <122925250+ritajogi@users.noreply.github.com>
2023-05-17 19:23:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
+import re
 import requests
-from bs4 import BeautifulSoup
 import telegram
 from telegram.ext import Updater, CommandHandler
 
+NETFLIX_API_URL = "https://www.netflix.com/api/shakti/{BUILD_IDENTIFIER}/pathEvaluator"
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the Netflix Image Scraper Bot! Send me a Netflix URL and I will scrape the poster image for you.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the Netflix Image Scraper Bot! Send me a Netflix URL and I will fetch the poster image for you.")
 
 
 def scrape_image(update, context):
     url = update.message.text.split()[1]
 
-    # Send a GET request to the Netflix URL and parse the HTML content
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    # Extract the Netflix ID from the URL
+    match = re.search(r"(?:\/title\/|\/watch\/)(\d+)", url)
+    if match:
+        netflix_id = match.group(1)
+        build_identifier = get_build_identifier()
+        if build_identifier:
+            image_url = fetch_poster_image(netflix_id, build_identifier)
+            if image_url:
+                context.bot.send_photo(chat_id=update.effective_chat.id, photo=image_url)
+                return
 
-    # Find the image tag in the HTML
-    image_tag = soup.find('img', {'class': 'hero-image.hero-image-desktop'})
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I couldn't fetch the Netflix poster image from the provided URL.")
 
-    if image_tag:
-        image_url = image_tag['src']
 
-        # Send the image as a reply
-        context.bot.send_photo(chat_id=update.effective_chat.id, photo=image_url)
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I couldn't find the Netflix poster image on the provided URL.")
+def get_build_identifier():
+    response = requests.get("https://www.netflix.com")
+    match = re.search(r"BUILD_IDENTIFIER\":\"([^ ]+)", response.text)
+    if match:
+        return match.group(1)
+
+    return None
+
+
+def fetch_poster_image(netflix_id, build_identifier):
+    headers = {
+        "Referer": f"https://www.netflix.com/title/{netflix_id}",
+        "x-netflix-maturity": "com.netflix.maturity.violence_rating=5&com.netflix.maturity.rating=12&com.netflix.maturity.audience=4&com.netflix.maturity.isMature=false",
+    }
+    payload = {
+        "withSize": "false",
+        "materialize": "true",
+    }
+    api_url = NETFLIX_API_URL.replace("{BUILD_IDENTIFIER}", build_identifier)
+    response = requests.post(api_url, headers=headers, params=payload)
+    data = response.json()
+    image_url = data.get("value", {}).get("image")
+    return image_url
 
 
 def main():
@@ -36,7 +60,7 @@ def main():
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
 
-    scrape_image_handler = CommandHandler('scrape', scrape_image)
+    scrape_image_handler = CommandHandler('scrape_image', scrape_image)
     dispatcher.add_handler(scrape_image_handler)
 
     # Start the bot
